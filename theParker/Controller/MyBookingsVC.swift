aaaf6858d38dc736124ec2e99771e4ee911644d1
ketanchoyal <sharmaketanr@gmail.com>
2019-03-18 00:16:49 +0530

    Optimization

---
+++ theParker/Controller/MyBookingsVC.swift
@@ -0,0 +1,54 @@
+//
+//  MyBookingsVC.swift
+//  theParker
+//
+//  Created by Ketan Choyal on 18/03/19.
+//  Copyright © 2019 Ketan Choyal. All rights reserved.
+//
+
+import UIKit
+
+class MyBookingsVC: UIViewController {
+    
+    @IBOutlet weak var menu: UIBarButtonItem!
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menu.target = revealViewController()
+        menu.action = #selector(SWRevealViewController.revealToggle(_:))
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
+
+extension MyBookingsVC {
+    
+    
+    func alert(message:String )
+    {
+        let alertview = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alertview.addAction(UIAlertAction(title: "Try Again!", style: .default, handler: {
+            action in
+            DispatchQueue.main.async {
+                
+                //  self.UISetup(enable: true)
+            }
+        }))
+        self.present(alertview, animated: true, completion: nil)
+        
+    }
+}
